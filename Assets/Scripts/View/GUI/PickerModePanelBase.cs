9a985f279986b559b2423771dee8636669e31ef8
towa-hi <yamyamasunday@gmail.com>
2020-05-04 03:58:19 -0700

    comments

--- Assets/Scripts/View/GUI/PickerModePanelBase.cs
+++ Assets/Scripts/View/GUI/PickerModePanelBase.cs
@@ -15,9 +15,11 @@ public class PickerModePanelBase : SerializedMonoBehaviour {
 
     // TODO: figure out why clicking on the scroll bar makes everything spaz out
     void Start() {
-        // load 
+        // load all the entitySchemas in the ScriptableObjects folder
         List<EntitySchema> schemaList = Resources.LoadAll("ScriptableObjects/Entities", typeof(EntitySchema)).Cast<EntitySchema>().ToList();
+        // sort by name
         schemaList.OrderBy(entitySchema => entitySchema.name.ToLower());
+        // instantiate a pickerItem for each schema
         foreach (EntitySchema entitySchema in schemaList) {
             GameObject pickerItem = Instantiate(pickerItemMaster, content.transform);
             PickerItemBase pickerItemBase = pickerItem.GetComponent<PickerItemBase>();
@@ -28,16 +30,18 @@ public class PickerModePanelBase : SerializedMonoBehaviour {
     }
 
     public void OnPickerModeSearchFieldValueChange(string aString) {
-        print(aString);
+        // if empty
         if (aString.Length == 0) {
             // show all
             this.contentList = this.fullEntityList;
         } else {
-            this.contentList = fullEntityList.Where(x => x.text.text.ToLower().Contains(aString.ToLower())).ToList();
+            // filter fullEntityList
+            this.contentList = this.fullEntityList.Where(x => x.text.text.ToLower().Contains(aString.ToLower())).ToList();
         }
         SetContent();
     }
-
+    
+    // hides pickerItems not in contentList
     public void SetContent() {
         foreach(PickerItemBase pickerItem in this.fullEntityList) {
             pickerItem.gameObject.SetActive(false);
