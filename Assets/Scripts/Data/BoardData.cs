c84b4ff4c2f6e35b41e95821e1a0955e2a7c0672
towa-hi <yamyamasunday@gmail.com>
2020-05-05 12:25:46 -0700

    last commit b efore big change

---
+++ Assets/Scripts/Data/BoardData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class BoardData : SingletonScriptableObject<BoardData> {
+
+    public GameGrid gameGrid;
+    public List<EntityBase> entityList;
+
+    public string title;
+    public string creator;
+    public int par;
+    public Vector2Int size;
+
+    public int attempts;
+
+    void Init() {
+
+    }
+
+    // destroy all the gameobjects and start over with new levelData
+    public void InitializeLevel(LevelData aLevelData) {
+        foreach (EntityBase entityBase in this.entityList) {
+            Destroy(entityBase.gameObject);
+        }
+        this.gameGrid = new GameGrid(aLevelData.levelSchema.size);
+        this.entityList = new List<EntityBase>();
+
+        this.title = aLevelData.levelSchema.title;
+        this.creator = aLevelData.levelSchema.creator;
+        this.par = aLevelData.levelSchema.par;
+        this.size = aLevelData.levelSchema.size;
+
+        this.attempts = aLevelData.attempts;
+    }
+
+
+}
---
+++ Assets/Scripts/SingletonScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+// singletons can be called with SingletonName.Instance
+public abstract class SingletonScriptableObject<T> : SerializedScriptableObject where T : SerializedScriptableObject {
+    static T _instance = null;
+    public static T Instance {
+        get {
+            if (!_instance) {
+                _instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            }
+            return _instance;
+        }
+    }
+}
